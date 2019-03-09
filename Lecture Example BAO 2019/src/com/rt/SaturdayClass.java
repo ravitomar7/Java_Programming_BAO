dd11a91093d16397b6aba3ce4fab1e3ddd2fa6e7
ravitomar <ravitomar7@gmail.com>
2019-03-09 12:48:56 +0530

    Commit to show usablity in eclipse.

---
+++ Lecture Example BAO 2019/src/com/rt/SaturdayClass.java
@@ -0,0 +1,15 @@
+package com.rt;
+
+public class SaturdayClass {
+
+	public SaturdayClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
